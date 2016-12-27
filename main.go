6543bc31f577333fe700fa5a0518a2a4cb78a30c
KoanJan <j.kehong@gmail.com>
2016-12-27 18:35:21 +0800

    WIP

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"mlt/lib"
+	"mlt/methods"
 )
 
 func main() {
-	a := lib.NewMatrixFloat64([][]float64{
-		[]float64{1.01, 2.4, 3.2},
-		[]float64{4.16, 5.31, 6.5},
-	})
-	b := lib.NewMatrixFloat64([][]float64{
-		[]float64{1.01, 4.16},
-		[]float64{2.4, 5.31},
-		[]float64{3.2, 6.5},
-	})
-	fmt.Println(a.Multi(b))
+	// train
+	var (
+		xSet []lib.Vector = []lib.Vector{
+			lib.NewVectorFloat64([]float64{-2.0, -0.9}), // 1
+			lib.NewVectorFloat64([]float64{-1.5, -2.5}), // 1
+			lib.NewVectorFloat64([]float64{-1.0, -0.5}), // 1
+			lib.NewVectorFloat64([]float64{-0.5, -0.5}), // 1
+			lib.NewVectorFloat64([]float64{0.1, -0.5}),  // 1
+			lib.NewVectorFloat64([]float64{0.5, -0.6}),  // 1
+			lib.NewVectorFloat64([]float64{1.1, 1.2}),   // -1
+			lib.NewVectorFloat64([]float64{1.5, 6.5}),   // -1
+			lib.NewVectorFloat64([]float64{2.2, 4.3}),   // -1
+		}
+		ySet []int = []int{1, 1, 1, 1, 1, 1, -1, -1, -1}
+	)
+	perceptron := methods.NewBasePerceptron(lib.NewVectorFloat64([]float64{-9, 3}), -5, 1)
+	perceptron.Train(xSet, ySet)
+
+	// test
+	// dst: w=(-1, 1) b=0
+	var uk lib.Vector
+	for i := 0; i < len(xSet); i++ {
+
+		uk = xSet[i]
+		r, e := perceptron.Predict(uk)
+		if e != nil {
+			log.Printf("error: %s\n", e.Error())
+		} else {
+			log.Printf("s: %v r: %d\n", uk, r)
+		}
+	}
 }
---
+++ methods/perceptron.go
@@ -0,0 +1,85 @@
+package methods
+
+import (
+	"fmt"
+	"log"
+
+	"mlt/lib"
+)
+
+// y = sign(w*x+b)
+type BasePerceptron struct {
+	w lib.Vector
+	b float64
+
+	eta float64
+}
+
+func signal(input interface{}) int {
+	switch input.(type) {
+	case float64:
+		v := input.(float64)
+		if v > 0 {
+			return 1
+		} else if v < 0 {
+			return -1
+		}
+	case int64:
+		v := input.(int64)
+		if v > 0 {
+			return 1
+		} else if v < 0 {
+			return -1
+		}
+	}
+	return 0
+}
+
+// training  (treat as float64)
+func (this *BasePerceptron) Train(xSet []lib.Vector, ySet []int) {
+	if len(xSet) == 0 || len(xSet) != len(ySet) {
+		return
+	}
+	for i := 0; i < len(xSet); i++ {
+		y, err := this.Predict(xSet[i])
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+		for y != ySet[i] {
+			// var _eta float64 = this.eta
+			// if y < ySet[i] {
+			// 	_eta = -this.eta
+			// }
+			log.Printf("w=%v b=%10f x=%v\n", this.w, this.b, xSet[i])
+			// wrong, fix w and b
+			tmpW := this.w.Plus(xSet[i].MultiNumber(this.eta * float64(ySet[i])))
+			if tmpW == nil {
+				log.Printf("w: %v x: %v eta: %5f y: %d\n", this.w, xSet[i], this.eta, ySet[i])
+				continue
+			} else {
+				this.w = tmpW
+				this.b = this.b + this.eta*float64(ySet[i])
+			}
+			y, err = this.Predict(xSet[i])
+			if err != nil {
+				log.Println(err.Error())
+				break
+			}
+		}
+	}
+}
+
+func (this *BasePerceptron) Predict(x lib.Vector) (y int, err error) {
+	tmp := this.w.Multi(x)
+	if tmp == nil {
+		err = fmt.Errorf("invalid dimensions of %v\n", x.Values())
+	} else {
+		y = signal(tmp.(float64) + this.b)
+	}
+	return
+}
+
+func NewBasePerceptron(w lib.Vector, b, eta float64) *BasePerceptron {
+	return &BasePerceptron{w: w, b: b, eta: eta}
+}
